9e4165885eb67b266ec7a89e093675dfe7973cd5
shikai.liu <shikai.liu@ly.com>
2020-08-25 18:05:04 +0800

    add the task

---
+++ src/main/java/com/lsk/backend/Main.java
@@ -0,0 +1,20 @@
+package com.lsk.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+*
+ * @Author shikai.liu
+ * @Description //TODO
+ * @Date 6:01 下午 2020/8/25
+ * @Param 
+ * @return 
+ **/
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}

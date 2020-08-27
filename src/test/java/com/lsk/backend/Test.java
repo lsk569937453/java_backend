c8d5977de880eeb4d4a4e8369cf2a66c5d4566d9
shikai.liu <shikai.liu@ly.com>
2020-08-27 09:13:12 +0800

    add the test

---
+++ src/test/java/com/lsk/backend/Test.java
@@ -0,0 +1,10 @@
+package com.lsk.backend;
+
+/**
+ * @ClassName : Test  //className
+ * @Description :   //description
+ * @Author : shikai.liu  //author
+ * @Date: 2020-08-27 09:13  //Date
+ */
+public class Test {
+}
